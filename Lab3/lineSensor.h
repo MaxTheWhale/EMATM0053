9ab29b0e43041f7019d62121c5b0a57f150f178f
Max Whale <maxwhale99@gmail.com>
2021-02-12 19:12:47 +0000

    Initial commit

---
+++ Lab3/lineSensor.h
@@ -0,0 +1,65 @@
+#ifndef _LINESENSOR_H
+#define _LINESENSOR_H
+
+class lineSensor_c {
+  
+  public:
+
+    int pin;
+    int bias = 0;
+
+    // Constructor, accepts a pin number as
+    // argument and sets this as input.
+    lineSensor_c( int which_pin ) {
+
+       // Record which pin we used.
+       pin = which_pin;
+
+       // Set this pin to input.
+       pinMode( pin, INPUT );
+    };
+
+
+    // Write your calibration routine here
+    // to remove bias offset
+    void calibrate() {
+      float value = 0;
+
+      // To help get you started, we use 
+      // the pin set/recorded by the 
+      // constructor to do an analogRead.
+      for (int i = 0; i < 100; i++) {
+        value += analogRead( pin ); 
+      }
+      
+      value /= 100;
+      bias = value;
+    }
+
+    int read() {
+      return analogRead( pin ) - bias;
+    }
+
+    int readRaw() {
+      return analogRead( pin );
+    }
+
+
+    // Write a routine here to check if your
+    // sensor is on a line (true or false).
+    boolean onLine( float threshold ) {
+
+      // if () {
+      //    return true;
+      //}
+             
+      return false;
+    }
+
+    // You can define other functions for
+    // yourself. 
+    // ...
+
+};
+
+#endif
---
+++ Lab3/motor.h
@@ -0,0 +1,49 @@
+#ifndef _MOTOR_H
+#define _MOTOR_H
+
+#define DIR_FWD LOW
+#define DIR_BWD HIGH
+
+// A class to neatly contain commands for the 
+// motors, to take care of +/- values, a min/max
+// power value, & pin setup.
+
+class motor_c {
+  int pin_dir;
+  int pin_pwm;
+  
+  public:
+
+    // This is a function prototype.
+    // The actual function is written outside
+    // of the class (see below).
+    motor_c( int which_pin_pwm, int which_pin_dir );
+    void setPower( int power );
+};
+
+// Constructor: when you create an instance
+// of this class, your constructor will be
+// called automatically.  You can treat it 
+// a bit like the main setup().  What needs
+// to be setup initially within this class?
+motor_c::motor_c( int which_pin_pwm, int which_pin_dir ) {
+  pin_pwm = which_pin_pwm;
+  pin_dir = which_pin_dir;
+  pinMode( pin_pwm, OUTPUT );
+  pinMode( pin_dir, OUTPUT );
+}
+
+void motor_c::setPower( int power ) {
+  if (power > 255 || power < -255) {
+    Serial.println("motor_c::setPower: bad power value");
+    return;
+  }
+  
+  int dir = (power < 0) ? DIR_BWD : DIR_FWD;
+  if (power < 0) power = -power;
+  
+  digitalWrite( pin_dir, dir );
+  analogWrite( pin_pwm, power );
+}
+
+#endif
